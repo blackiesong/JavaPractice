2a2d83d61b19edf2e4c6830122ee79a3dbbd6ada
blackiesong <277464327@qq.com>
2017-07-17 16:12:10 +0800

    内部类继承

---
+++ src/summer/Innerclothes.java
@@ -0,0 +1,30 @@
+package summer;
+
+import java.lang.ProcessBuilder.Redirect;
+
+public class Innerclothes {
+	class clothes{
+		private String material;
+		private int size;
+		private String color;
+		public String getColor() {
+			return color;
+		}
+		public void setColor(String color){
+			this.color=color;
+		}
+		
+	}
+	static class coat extends Innerclothes.clothes{
+		public coat(Innerclothes o) {
+			o.super();
+		}
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Innerclothes o=new Innerclothes();
+		coat s=new coat(o);
+		s.setColor("red");
+		System.out.println("ÑÕÉ«ÊÇ£º"+s.getColor());
+	}
+}
