1036df868e129f95ad949ff1399eeec83d5d1bcb
blackiesong <277464327@qq.com>
2017-07-23 21:01:15 +0800

    测试整数型缓存（常量池概念）

---
+++ src/summer/TestIntegerCache.java
@@ -0,0 +1,16 @@
+package summer;
+
+public class TestIntegerCache {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer i3=Integer.valueOf(100);
+		Integer i4=Integer.valueOf(100);
+		System.out.println(i3==i4);
+		Integer i5=Integer.valueOf(1000);
+		Integer i6=Integer.valueOf(1000);
+		System.out.println(i5==i6);
+		
+	}
+
+}
